2d3e6928b705b0243932b3655712023c9ecfe886
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-03-05 10:20:10 +0900

    Vue 설치

---
+++ vue_app1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <HelloWorld message="안녕하세요 반갑습니다. Vue 세상"/> -->
+  <MyCheckbox3 />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import MyCheckbox3 from './components/MyCheckbox3.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    MyCheckbox3,
+  }
+}
+</script>
+
+<style>
+#app {
+  border: 1px solid;
+  text-align: center;
+  padding: 30px;
+}
+</style>
