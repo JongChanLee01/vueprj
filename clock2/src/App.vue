752c59eb5436d08179941f75002971835bf694f0
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-03-05 15:35:42 +0900

    메모리 누수 방지

--- clock2/src/App.vue
+++ clock2/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <h1>시계</h1>
-    <MyClock />
+    <MyClock v-if="showTimer" class="clockStyle1" format="YYYY-MM-DD HH:mm:ss" />
+    <MyClock v-if="showTimer" class="clockStyle2" />
+    <div>
+      <button type="button" @click="showTimer=true" :disabled="showTimer">시계보이기</button>
+      <button type="button" @click="showTimer=false" :disabled="!showTimer">시계감추기</button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +15,11 @@ import MyClock from './MyClock.vue'
 
 export default {
   name: 'App',
+  data: function() {
+    return{
+      showTimer:true
+    }
+  },
   components: {
     MyClock
   }
@@ -24,4 +34,11 @@ export default {
     border: 1px solid #ccc;
     box-shadow: 3px 3px 3px #aaa;
   }
+  button {
+    padding: 0.5em 1.5em;
+    margin: 20px 5px 0 0;
+  }
+  .clockStyle1, .clockStyle2 { font-size: 15pt; border: 1px solid gray; }
+  .clockStyle1 { padding: 3px 12px; margin-right: 5px; background-color: #ffa; }
+  .clockStyle2 { padding: 3px 30px; background-color: #bfb;  }
 </style>
--- clock2/src/MyClock.vue
+++ clock2/src/MyClock.vue
@@ -8,13 +8,20 @@
 import moment from "moment";
     export default {
         name: "MyClock",
+        props: {
+            format: {
+                type: String,
+                default: "HH:mm:ss"
+            }
+        },
         data() {
             return { time: null };
         },
         mounted() {
             // 콜백 함수를 만들고
             let callback=()=> {
-                this.time = moment().format("HH:mm:ss");
+                // this.time = moment().format("HH:mm:ss");
+                this.time = moment().format(this.format);
                 console.log(this.time);
             }
             
@@ -22,7 +29,12 @@ import moment from "moment";
             callback();
 
             // 1초 간격으로 콜백함수 반복 호출
-            setInterval(callback, 1000);
+            // setInterval(callback, 1000);
+            this.timerId = setInterval(callback, 1000);
+        },
+        // 메모리 누수되는거 막아줌
+        beforeUnmount() {
+            clearInterval(this.timerId);
         }
     }
 </script>
